a3112a560dfb440f68af82b0f4f75e58ecf15828
maestrOoOo <mma.programmer@gmail.com>
2024-01-03 15:18:12 +0200

    login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using API.DB.Repositories;
+using API.Models;
+using API.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors]
+    public class AccountController : ControllerBase
+    {
+        private readonly IRepository<Customer> _repo;
+
+        public AccountController(IRepository<Customer> repo)
+        {
+            _repo = repo;
+        }
+        [HttpPost]
+        public async Task<ActionResult<AccountVM>> Login(AccountVM user)
+        {
+           
+            var old = await _repo.GetAsync(x => x.Mobile == user.Mobile);
+            if (old.Any())
+            {
+                var currentUser = old.FirstOrDefault();
+                user.Name = currentUser.Name;
+                user.Id = currentUser.Id;
+                user.Email = currentUser.Email;
+                return user;
+            }
+
+            ModelState.AddModelError("Mobile", "this number not exist");
+            return BadRequest(ModelState);
+
+        }
+    }
+}
---
+++ ViewModels/AccountVM.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.ViewModels
+{
+    public class AccountVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
